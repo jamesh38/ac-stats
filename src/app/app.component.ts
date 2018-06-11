bfa2fde535246497ef18b51617d8f812b0007bda
jamesh38 <james.hoegerl@gmail.com>
2018-06-10 23:29:13 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'acstats-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'acstats';
+}
