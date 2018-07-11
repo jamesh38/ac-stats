fef55207e07246357e7a80e226ca113dcc603cff
jamesh38 <james.hoegerl@gmail.com>
2018-07-11 03:40:03 -0400

    do we have it?

--- deploy.js
+++ deploy.js
@@ -3,45 +3,48 @@ var path = require("path");
 var fs = require('fs');
 const mime = require('mime-types')
 
-// const uploadDir = function(s3Path, bucketName) {
+function uploadDir(s3Path, bucketName) {
 
-//     let s3 = new AWS.S3();
-//     let cloudfront = new AWS.CloudFront();
-//     let files = [];
+    let s3 = new AWS.S3();
+    let files = [];
 
-//     function walkSync(currentDirPath, callback) {
-//         fs.readdirSync(currentDirPath).forEach(function (name) {
-//             var filePath = path.join(currentDirPath, name);
-//             var stat = fs.statSync(filePath);
-//             if (stat.isFile()) {
-//                 callback(filePath, stat);
-//             } else if (stat.isDirectory()) {
-//                 walkSync(filePath, callback);
-//             }
-//         });
-//     }
+    function walkSync(currentDirPath, callback) {
+        fs.readdirSync(currentDirPath).forEach(function (name) {
+            var filePath = path.join(currentDirPath, name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walkSync(filePath, callback);
+            }
+        });
+    }
 
-//     walkSync(s3Path, function(filePath, stat) {
-//         let fileName = filePath.split("\\")[2];
-//         files.push(fileName);
-//         let params = {
-//             Bucket: bucketName,
-//             Key: fileName,
-//             Body: fs.readFileSync(filePath),
-//             ACL: "public-read",
-//             ContentType: mime.lookup(filePath)
-//         };
-//         s3.putObject(params, function(err, data) {
-//             if (err) {
-//                 throw new Error(err);
-//             } else {
-//                 console.log("Successfully uploaded " + fileName + " to " + bucketName);
-//             }
-//         });
+    walkSync(s3Path, function(filePath, stat) {
+        let fileName = filePath.split("\\")[2];
+        files.push(fileName);
+        let params = {
+            Bucket: bucketName,
+            Key: fileName,
+            Body: fs.readFileSync(filePath),
+            ACL: "public-read",
+            ContentType: mime.lookup(filePath)
+        };
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                throw new Error(err);
+            } else {
+                console.log("Successfully uploaded " + fileName + " to " + bucketName);
+            }
+        });
 
-//     });
+    });
 
-// };
+    setTimeout(() => {
+        createInvalidation();
+    }, 5000);
+
+};
 
 function pollInvalidation(tries, maxTries, data) {
     tries++;
@@ -71,9 +74,46 @@ function pollInvalidation(tries, maxTries, data) {
     }, 5000);
 }
 
-function createInvalidation() {
+function listAllKeys(token, cb) {
+    let s3 = new AWS.S3();
+    let allKeys = [];
+    let opts = { Bucket: 'acstats' };
+    if (token) opts.ContinuationToken = token;
+
+    s3.listObjectsV2(opts, function (err, data) {
+        allKeys = allKeys.concat(data.Contents);
+
+        if (data.IsTruncated)
+            listAllKeys(data.NextContinuationToken, cb);
+        else
+            cb(allKeys);
+    });
+}
+
+function deleteFilesInBucket() {
+    let s3 = new AWS.S3();
+    listAllKeys(null, keys => {
+        s3.deleteObjects({
+            Bucket: 'acstats',
+            Delete: {
+                Objects: keys.map(item => {
+                    return {Key: item.Key};
+                })
+            }
+        }, (err, data) => {
+            if(err) {
+                throw new Error(err);
+            } else {
+                console.log(data);
+                uploadDir("./dist/ac-stats", "acstats");
+            }
+        });
+    });
+}
+
+function createInvalidation(files) {
     let cloudfront = new AWS.CloudFront();
-    cloudfront.createInvalidation({
+    var params = {
         DistributionId: 'E1V6PC9HA59YI1',
         InvalidationBatch: {
             CallerReference: '' + new Date().getTime(),
@@ -82,7 +122,8 @@ function createInvalidation() {
                 Items: ['/*']
             }
         }
-    } , function (err, data) {
+    };
+    cloudfront.createInvalidation(params, function (err, data) {
         if (err) {
             throw new Error(err);
         }
@@ -94,5 +135,4 @@ function createInvalidation() {
     });
 }
 
-// uploadDir("./dist/ac-stats", "acstats");
-createInvalidation();
+deleteFilesInBucket();
