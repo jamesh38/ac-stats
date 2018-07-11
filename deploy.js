6636b78b93ad3450987fd6f1731ec4bb7a5fdb50
jamesh38 <james.hoegerl@gmail.com>
2018-07-11 02:48:43 -0400

    ugh

--- deploy.js
+++ deploy.js
@@ -3,68 +3,45 @@ var path = require("path");
 var fs = require('fs');
 const mime = require('mime-types')
 
-const uploadDir = function(s3Path, bucketName) {
+// const uploadDir = function(s3Path, bucketName) {
 
-    let s3 = new AWS.S3();
-    let cloudfront = new AWS.CloudFront();
-    let files = [];
+//     let s3 = new AWS.S3();
+//     let cloudfront = new AWS.CloudFront();
+//     let files = [];
 
-    function walkSync(currentDirPath, callback) {
-        fs.readdirSync(currentDirPath).forEach(function (name) {
-            var filePath = path.join(currentDirPath, name);
-            var stat = fs.statSync(filePath);
-            if (stat.isFile()) {
-                callback(filePath, stat);
-            } else if (stat.isDirectory()) {
-                walkSync(filePath, callback);
-            }
-        });
-    }
+//     function walkSync(currentDirPath, callback) {
+//         fs.readdirSync(currentDirPath).forEach(function (name) {
+//             var filePath = path.join(currentDirPath, name);
+//             var stat = fs.statSync(filePath);
+//             if (stat.isFile()) {
+//                 callback(filePath, stat);
+//             } else if (stat.isDirectory()) {
+//                 walkSync(filePath, callback);
+//             }
+//         });
+//     }
 
-    walkSync(s3Path, function(filePath, stat) {
-        let fileName = filePath.split("\\")[2];
-        files.push(fileName);
-        let params = {
-            Bucket: bucketName,
-            Key: fileName,
-            Body: fs.readFileSync(filePath),
-            ACL: "public-read",
-            ContentType: mime.lookup(filePath)
-        };
-        s3.putObject(params, function(err, data) {
-            if (err) {
-                throw new Error(err);
-            } else {
-                console.log("Successfully uploaded " + fileName + " to " + bucketName);
-            }
-        });
+//     walkSync(s3Path, function(filePath, stat) {
+//         let fileName = filePath.split("\\")[2];
+//         files.push(fileName);
+//         let params = {
+//             Bucket: bucketName,
+//             Key: fileName,
+//             Body: fs.readFileSync(filePath),
+//             ACL: "public-read",
+//             ContentType: mime.lookup(filePath)
+//         };
+//         s3.putObject(params, function(err, data) {
+//             if (err) {
+//                 throw new Error(err);
+//             } else {
+//                 console.log("Successfully uploaded " + fileName + " to " + bucketName);
+//             }
+//         });
 
-    });
+//     });
 
-
-    setTimeout(() => {
-        var params = {
-            DistributionId: 'E1V6PC9HA59YI1',
-            InvalidationBatch: {
-                CallerReference: '' + new Date().getTime(),
-                Paths: {
-                    Quantity: files.length,
-                    Items: files.map(item => "/" + item)
-                }
-            }
-        };
-        cloudfront.createInvalidation(params, function(err, data) {
-            if (err) {
-                throw new Error(err);
-            }
-            else{ 
-                console.log("Successfully invalidated.");
-                console.log("Waiting for invalidation to complete...");
-                pollInvalidation(0, 30, data);
-            }
-        });
-    }, 3000)
-};
+// };
 
 function pollInvalidation(tries, maxTries, data) {
     tries++;
@@ -94,4 +71,28 @@ function pollInvalidation(tries, maxTries, data) {
     }, 5000);
 }
 
-uploadDir("./dist/ac-stats", "acstats");
+function createInvalidation() {
+    let cloudfront = new AWS.CloudFront();
+    cloudfront.createInvalidation({
+        DistributionId: 'E1V6PC9HA59YI1',
+        InvalidationBatch: {
+            CallerReference: '' + new Date().getTime(),
+            Paths: {
+                Quantity: 1,
+                Items: ['/*']
+            }
+        }
+    } , function (err, data) {
+        if (err) {
+            throw new Error(err);
+        }
+        else {
+            console.log("Successfully invalidated.");
+            console.log("Waiting for invalidation to complete...");
+            pollInvalidation(0, 30, data);
+        }
+    });
+}
+
+// uploadDir("./dist/ac-stats", "acstats");
+createInvalidation();
